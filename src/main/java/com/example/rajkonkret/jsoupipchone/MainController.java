be1337939dbeddaa3019b5077dbe73ba1ec0337c
rajkonkret <rajkonkret660@gmail.com>
2020-07-03 14:15:46 +0200

    add create user and findall()

--- src/main/java/com/example/rajkonkret/jsoupipchone/MainController.java
+++ src/main/java/com/example/rajkonkret/jsoupipchone/MainController.java
@@ -1,11 +1,11 @@
 package com.example.rajkonkret.jsoupipchone;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -14,12 +14,20 @@ public class MainController {
 
     @Autowired
     UserRepository userRepository;
-    @GetMapping("test")
-    public String getHello() {
-        User user  = new User("Radek");
-        userRepository.save(user);
 
-        return "hello";
+    @GetMapping("test")
+    public List<User> getHello() {
+       //ReGwiazda:) niedługo będzie livy jak Waldek robił i kosił 200 k na miesiąć;) User user  = new User("Radek", "Janiak");
+        System.out.println(LocalDate.now());
+        return userRepository.findAll();
+    }
+    @PostMapping("create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User saveUser(@RequestBody User user){
+
+        System.out.println(user);
+        return userRepository.save(user);
+
     }
 }
 
--- src/main/java/com/example/rajkonkret/jsoupipchone/User.java
+++ src/main/java/com/example/rajkonkret/jsoupipchone/User.java
@@ -1,14 +1,16 @@
 package com.example.rajkonkret.jsoupipchone;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
-
+@Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,9 +21,14 @@ public class User {
     Long id;
 
     String name;
+    String surname;
+    String pesel;
+    LocalDate dateOfVisit;
 
-    public User(String user) {
+
+    public User(String user, String surname) {
         this.name = user;
+        this.surname = surname;
     }
 
     public Long getId() {
@@ -38,5 +45,16 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", dateOfVisit=" + dateOfVisit +
+                '}';
     }
 }
